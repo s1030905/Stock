316824c6d72ced2b4c194191724126bd0c3e3054
s1030905 <s1030905@outlook.com>
2023-07-19 20:38:31 +0800

    feat: add stock link at home page

--- controllers/stock-controller.js
+++ controllers/stock-controller.js
@@ -184,13 +184,13 @@ const stockController = {
       // 與昨日價格差
       const diff = ["N/A"];
       for (let i = 1; i < date.length; i++) {
-        diff.push((price[0].close[i] - price[0].close[i - 1]).toFixed(2));
+        if (!price[0].close[i]) diff.push("N/A");
+        else diff.push((price[0].close[i] - price[0].close[i - 1]).toFixed(2));
       }
       // 輸出資料格式整理
       const data = {};
       const dic = await stockList(stockId);
       const title = dic[stockId].name;
-      console.log(price[0]);
       for (let i = 0; i < date.length; i++) {
         data[date[i]] = {
           date: date[i],
