2a2becfe9417dea1d574c454c8091e3763ffb4a2
s1030905 <s1030905@outlook.com>
2023-07-26 15:56:22 +0800

    feat: add index-close & fluctuations-percent sort

--- public/javascripts/sort-index.js
+++ public/javascripts/sort-index.js
@@ -1,13 +1,25 @@
 // 監聽 "col" 的點擊事件
 const btn = document.querySelector("#col");
 btn.addEventListener("click", async (event) => {
+  const response = await fetch(`/api/stock/index`);
+  const result = await response.json();
   if (event.target.id === "fluctuations") {
-    const response = await fetch(`/api/stock/index`);
-    const result = await response.json();
     let data = result["data"].sort((a, b) => {
       return (
         Number(a["漲跌"] + a["漲跌點數"]) - Number(b["漲跌"] + b["漲跌點數"])
       );
+    });
+    display(data);
+  }
+  if (event.target.id === "fluctuations-percent") {
+    let data = result["data"].sort((a, b) => {
+      return Number(a["漲跌百分比"]) - Number(b["漲跌百分比"]);
+    });
+    display(data);
+  }
+  if (event.target.id === "index-close") {
+    let data = result["data"].sort((a, b) => {
+      return Number(a["收盤指數"]) - Number(b["收盤指數"]);
     });
     display(data);
   }
@@ -16,7 +28,17 @@ const display = (data) => {
   const tableContainer = document.querySelector("#table-body");
   tableContainer.innerHTML = ``;
   for (let i = 0; i < data.length; i++) {
-    tableContainer.innerHTML += `
+    if (Math.abs(Number(data[i]["漲跌百分比"])) >= 2) {
+      tableContainer.innerHTML += `
+      <tr>
+        <th scope="row">${i + 1}</th>
+        <td>${data[i]["指數"]}</td>
+        <td>${data[i]["收盤指數"]}</td>
+        <td>${data[i]["漲跌"]} ${data[i]["漲跌點數"]}</td>
+        <td style="color: red;">${data[i]["漲跌百分比"]} %</td>
+      </tr>`;
+    } else {
+      tableContainer.innerHTML += `
       <tr>
         <th scope="row">${i + 1}</th>
         <td>${data[i]["指數"]}</td>
@@ -24,6 +46,7 @@ const display = (data) => {
         <td>${data[i]["漲跌"]} ${data[i]["漲跌點數"]}</td>
         <td>${data[i]["漲跌百分比"]}</td>
       </tr>`;
+    }
   }
 };
 // {{#ifCondition this.漲跌百分比}}{{/ifCondition}}
