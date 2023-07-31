1c0abf97c69f885eba5c7ade37221c56de0cead0
s1030905 <s1030905@outlook.com>
2023-07-31 16:38:53 +0800

    fix: RSI count error and explanation text

--- public/javascripts/kd-form.js
+++ public/javascripts/kd-form.js
@@ -91,7 +91,7 @@ const explanationBtn = document.querySelector("#explanationBtn");
 explanationBtn.addEventListener("mouseenter", (event) => {
   const explanationContainer = document.querySelector("#explanationContainer");
   explanationContainer.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
-    黃金交叉為買點；死亡交叉為買點
+    黃金交叉為買點；死亡交叉為賣點
   </div>`;
 });
 
